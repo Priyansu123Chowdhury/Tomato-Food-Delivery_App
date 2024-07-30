14b98573f65d07c1233384e58cf002d61fc1a546
Priyansu123Chowdhury <102274788+Priyansu123Chowdhury@users.noreply.github.com>
2024-07-30 22:30:28 +0530

    Direct Uplaod

---
+++ backend/config/db.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  await mongoose
+    .connect(
+      "mongodb+srv://Priyansu:Nehajan123@cluster0.fostq3e.mongodb.net/food-del"
+    )
+    .then(() => console.log("DB Connected"));
+};
+
+// add your mongoDB connection string above.
+// Do not use '@' symbol in your databse user's password else it will show an error.
